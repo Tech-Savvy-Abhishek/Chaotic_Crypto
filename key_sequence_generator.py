6610ba407313641b2973fb9948e07a91c08faea8
Tech-Savvy-Abhishek <abhishekgs999@outlook.com>
2024-01-26 18:40:04 +0530

    Add empty files for key sequence generator

---
+++ key_sequence_generator.py
@@ -0,0 +1,14 @@
+def generate_key_sequence(m):
+    # generate 6 integers
+    # sha 256
+    # H to B
+    # B to N
+    # Henon
+    # kaplan yorke
+    # xor
+    # reshape to matrix
+    # tinkerbell
+    # permute
+    # revert matrix reshape
+    km = 0
+    return km
